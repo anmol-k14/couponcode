a77da3807fd6baca3bfb680528e2aa967050ab63
anmol-k14 <anmolkumar.1403@gmail.com>
2025-03-14 20:51:40 +0530

    sdf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         try {     
             const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/coupons/claim`,{},
             //   { withCredentials: true }
-             );
+            );
             setCoupon(response.data.coupon);
             setMessage(response.data.message);
         } catch (error) {
